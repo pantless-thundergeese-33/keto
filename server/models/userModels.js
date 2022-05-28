de25250f3e5b5466b9bc28fb6ee109916929f509
Paul Yi <paulyiengr@gmail.com>
2022-05-28 14:12:03 -0400

    added code for controller, api, and mongodb connection

---
+++ server/models/userModels.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const MONGO_URI = 'mongodb+srv://paulyiengr:codesmithtemppw@cluster1.zmttq.mongodb.net/?retryWrites=true&w=majority';
+
+
+mongoose.connect(MONGO_URI, {
+  // Options for parsing the URI (not too sure what's going on here, it's just from the star wars database unit)
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'ketousers'
+})
+  .then(console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+// Collections
+
+// Activities
+// Collections: electricity / car / flight / shipping etc.
+//  user ObjectId, inputs (electricity/car/flight/shipping), footprint(carbon-lbs), footprint(carbon-mt), time of request)
+
+const activitySchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  activityType: String,
+  details: {},
+  footprintLb: Number,
+  footprintMt: Number,
+  reqTime: Date
+});
+
+
+// users      
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  activities:{
+      activityId: {
+        type: Schema.Types.ObjectId,
+        ref: 'activity'
+      }
+  },
+});
+
+const User = mongoose.model('user',userSchema);
+const Activity = mongoose.model('activity',activitySchema);
+
+module.exports = {
+  User,
+  Activity  
+};
+//  ObjectId(default), username, password, { activityType, activityId }, 
