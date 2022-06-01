596acfb903b6144a7665a8ba1528e53e08011ddd
Parkreiner <throwawayclover@gmail.com>
2022-06-01 11:41:30 -0400

    refactor: Rewrite the Login.jsx file. Logic should be roughly equivalent, but now the file uses useState hooks instead of referencing the DOM directly, and labels have been added to <input> elements.

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,57 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Route, Navigate, useNavigate } from 'react-router-dom';
 
-const Login = (props) => {
+/**
+ * The current version of Login uses Alert messages for debugging. You should absolutely, 100%
+ * remove these from a production app. Using alerts are bad UX.
+ */
+const Login = ({ addUser, setUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
-  const loginBtnHelper = async () => {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const response = await fetch(`/api/login?username=${username}&password=${password}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.msg) {
-          alert('incorrect username and/or password');
-        } else {
-          props.setUser(document.getElementById('username').value);
-          navigate('/login');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
-
-  const createAccountBtnHelper = async () => {
-    const response = await fetch('/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: document.getElementById('username').value,
-        password: document.getElementById('password').value,
-      }),
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    await props.addUser(response);
-    navigate('/login');
-  };
 
   return (
     <div className="login">
       <h3>Log In</h3>
-      <input id="username" type="text" placeholder="username" />
-      <input id="password" type="password" placeholder="password" />
+
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
       <div className="signInButtons">
-        <button onClick={loginBtnHelper}>Log In</button>
-        <button onClick={createAccountBtnHelper}>Create Account</button>
+        <button onClick={handleLogin} onBlur={handleLogin}>
+          Log In
+        </button>
+
+        <button onClick={handleAccountCreation} onBlur={handleAccountCreation}>
+          Create Account
+        </button>
       </div>
     </div>
   );
+
+  ////////// Start of internal helpers
+
+  async function handleLogin() {
+    try {
+      const response = await fetch(`/api/login?username=${username}&password=${password}`);
+      const json = await response.json();
+
+      if (!json || !json.msg) {
+        alert('incorrect username and/or password');
+        return;
+      }
+
+      setUser(username);
+      navigate('/login');
+    } catch (err) {
+      console.error(err instanceof Error ? err.stack : err);
+    }
+  }
+
+  async function handleAccountCreation() {
+    try {
+      const response = await fetch('/api', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Server is currently sending string value directly
+      const jsonResponse = response.json();
+      if (typeof jsonResponse !== 'string' || jsonResponse.length === 0) {
+        alert("Server didn't send back response data correctly.");
+        return;
+      }
+
+      addUser(response);
+      navigate('/login');
+    } catch (err) {
+      console.error(err instanceof Error ? err.stack : err);
+    }
+  }
 };
 
 export default Login;
--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,9 +1,5 @@
 export const SET_USER = 'SET_USER';
-
 export const SET_GUEST = 'SET_GUEST';
-
 export const ADD_USER = 'ADD_USER';
-
 export const SET_ACTIVITY = 'SET_ACTIVITY';
-
 export const UPDATE_CARBON = 'UPDATE_CARBON';
