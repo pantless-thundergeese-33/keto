883a2bdfb1d3641260212555fabb171b8f25697c
Parkreiner <throwawayclover@gmail.com>
2022-06-01 11:58:07 -0400

    refactor: Update Guest.jsx and styling to follow better industry practices.

--- client/components/Guest.jsx
+++ client/components/Guest.jsx
@@ -4,10 +4,8 @@ import { Link } from 'react-router-dom';
 const Guest = (props) => {
   return (
     <div className="guest">
-      <Link to={'/home'}>
-        <button className="guestBtn" onClick={props.setGuest}>
-          Continue as Guest
-        </button>
+      <Link to={'/home'} onClick={props.setGuest} className="guest-link-btn">
+        Continue as Guest
       </Link>
     </div>
   );
